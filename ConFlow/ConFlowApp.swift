cdf96287e72c63db026ae7d9735a722b96cb09f6
jtham <jtham@iu.edu>
2024-07-14 22:07:29 -0500

    Initial Commit

---
+++ ConFlow/ConFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConFlowApp.swift
+//  ConFlow
+//
+//  Created by Joseph Tham on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ConFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
